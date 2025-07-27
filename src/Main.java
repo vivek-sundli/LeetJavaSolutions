52d59af981da2f266c3593977f79c029ca3ca217
vivek-sundli <viveksundli@github.com>
2025-07-27 18:23:49 +0530

    adding missing and repeating number solution

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,7 @@ import java.util.*;
 
 
 public class Main{
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
-        Set<List<Integer>> st = new HashSet<>();
-        //        int[] nums = {1, -2, 3, 5, 7, 9};
-        //        int target = 7;
-        for(int i=0 ; i<nums.length; i++){
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            for(int j=i+1 ; j<nums.length ; j++){
-                int complement = target-nums[i]-nums[j];
-                int right = nums.length-1;
-                int left = j+1;
 
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-                while(left < right){
-                    if(complement == nums[left]+nums[right]){
-                        List<Integer> temp = Arrays.asList(nums[left],nums[right],nums[i],nums[j]);
-                        Collections.sort(temp);
-                        st.add(temp);
-                        while(left < right && nums[left] == nums[left+1]){
-                            left++;
-                        }
-                        while(left < right && nums[right] == nums[right-1]){
-                            right--;
-                        }
-                    }
-                    if(complement < nums[left]+nums[right]){
-                        right--;
-                    } else {
-                        left++;
-                    }
-                }
-            }
-        }
-        List<List<Integer>> ans = new ArrayList<>();
-        for(List<Integer> x : st){
-            ans.add(x);
-        }
-        return ans;
-    }
 
     public static void main(String[] args){
         //write input for nextPermutation from Solution class
--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,7 @@
+import java.util.*;
+
 class Solution {
+
     public void swap(int[] nums, int i,int j){
         int temp = nums[i];
         nums[i] = nums[j];
@@ -38,5 +41,79 @@ class Solution {
         //swap both
         swap(nums, pivot, smallestIndex);
         reverse_array(nums, pivot + 1, n - 1);
+    }
+
+    //complete the function od SumNumbers retruning the sum of all numbers in the array
+    public int sumNumbers(int[] nums){
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+    //complete the function of Sum1stNumbers returning the sum of first n natural numbers
+    public int sum1stNumbers(int[] nums){
+        int n = nums.length;
+        return (n * (n + 1)) / 2;
+    }
+    //complete the function of SumSquare returning the sum of squares in the array
+    public int sumSquare(int[] nums){
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i] * nums[i];
+        }
+        return sum;
+    }
+    //complete the function of Sum1stSqaure returning the sum of squares of first n natural numbers
+    public int sum1stSqaure(int[] nums){
+        int n = nums.length;
+        return (n * (n + 1) * (2 * n + 1)) / 6;
+    }
+    public int[] missingAndRepeating(int[] nums){
+        int Sum = sumNumbers(nums);
+        int Sum_1stN_NaturalNumbers = sum1stNumbers(nums);
+        int Sum_SquareNaturalNumbers = sumSquare(nums);
+        int Sum_1stN_SqiareNatualNumbers = sum1stSqaure(nums);
+        int val1 = Sum_1stN_NaturalNumbers - Sum;
+        int val2 = Sum_1stN_SqiareNatualNumbers/Sum_SquareNaturalNumbers;
+        int repeating = (val1 + val2) / 2;
+        int missing = val2 - repeating;
+        return new int[]{missing, repeating};
+    }
+    // complete the function of fourSum returning all unique quadruplets in the array that sum up to the target
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // skip duplicates
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) left++; // skip duplicates
+                        while (left < right && nums[right] == nums[right - 1]) right--; // skip duplicates
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 }
