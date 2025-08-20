14d2648e03db22fce220a950dc12c9aaca02ae91
vivek-sundli <viveksundli@github.com>
2025-08-20 20:49:03 +0530

    plain old classes

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,7 @@ import java.util.*;
 
 public class Main{
 
-
     public static void main(String[] args){
-        //write input for nextPermutation from Solution class
-        Solution s = new Solution();
-        int[] nums1 = new int[]{1, 2, 3, 4, 5};
-        s.nextPermutation(nums1);
-        //print nums1
-        System.out.println(Arrays.toString(nums1));
 
     }
 }
